52ce90d0da8eb440c35e2576dae76afc45a4e916
320834 <justinchen1000@gmail.com>
2019-10-20 22:37:55 -0400

    Added rudimentary routes, added test cases for routes

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require("./routes/login");
+var databaseRouter = require('./routes/database');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/users', usersRouter);
+app.use('/database', databaseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
