157d09bbc751a51e7267240559c436ec3d3cf58d
shellycao123 <sc6472@nyu.edu>
2019-12-04 22:27:55 -0500

    add filter for class user want to add to the schedule

--- Backend/routes/schedule.js
+++ Backend/routes/schedule.js
@@ -23,7 +23,7 @@ router.post('/add', function(req, res, next){
       }
       else{
         //get all the courses the user currently have
-        courses = user.selected
+        const courses = user.selected
         //get course to be added
         Course.findById(add_id, function(err, toAdd){
           if(err){
@@ -50,10 +50,7 @@ router.post('/add', function(req, res, next){
           }
 
         })
-
-
       }
-
     })
 
 
@@ -83,6 +80,52 @@ router.get('/viewSchedule', function(req,res,next){
 
 //check if a course can be added to the course list
 function canBeAdd(courses, toAdd){
+  if(toAdd.time == "Room: TBA"){
+    return
+  }
+  //"TuTh 2:00PM - 3:15PM"
+  const addTime = splitTime(toAdd.time)
+  let comTime, dateSet
+  for(let i = 0 ; i < courses.length; i++){
+    if(courses[i].time != "Room: TBA"){
+      comTime = splitTime(courses[i].time)
+      dateSet = comTime.date.entries()
 
+      for(let date of dateSet){
+        if(addTime.date.has(date[0]) && !(addTime.end <= comTime.begin || addTime.begin > comTime.end)){
+          return new InvalidInputError('There is a conflict of the time you picked with your current schedule.')
+        }
+      }
+    }
+  }
 }
+function splitTime(time){
+  var result = {}
+  result.date = new Set([])
+  time = time.split(' ');
+  result.begin = parseTime(time[1])
+  result.end= parseTime(time[3])
+  for(let i = 0; i < time[0].length/2;i++){
+    result.date.add(time[0].substring(i * 2, i * 2 + 2))
+  }
+  return result
+}
+function parseTime(time){
+  newTime = time.substring(0,time.length - 2).split(':')
+  if(parseInt(time.substring(0,time.length - 2), 10) == 12){
+    if(time.substring(time.length - 2, time.length) == 'AM'){
+      return parseInt(newTime[1],10)
+    }
+    else{
+      return parseInt(newTime[0],10)*60 + parseInt(newTime[1],10)
+    }
+  }
+  if(time.substring(time.length - 2, time.length) == 'AM'){
+    return parseInt(newTime[0],10)*60 + parseInt(newTime[1],10)
+  }
+  else{
+    return parseInt(newTime[0],10)*60 + parseInt(newTime[1],10) + 12 * 60
+  }
+}
+
 module.exports = router;
