d35695352b917139c55a4e8e22bff2db3fe35af7
ZheyueZhao <zheyuezhao@gmail.com>
2019-12-08 21:31:56 -0500

    Update CourseData.js

--- frontend/src/components/CourseData.js
+++ frontend/src/components/CourseData.js
@@ -1,1 +1,19 @@
 
+import { CourseBuilder } from './course-builder';
+
+export class House {
+    title: string;
+    startDate: number;
+    endDate: number;
+    allDay: boolean;
+    id: number;
+    location: string;
+    constructor(CourseBuilder: CourseBuilder) {
+        this.title = CourseBuilder.title;
+        this.startDate = CourseBuilder.startDate;
+        this.endDate = CourseBuilder.endDate;
+        this.allDay = CourseBuilder.allDay;
+        this.id = CourseBuilder.id;
+         this.location: CourseBuilder.location;
+    }
+}
