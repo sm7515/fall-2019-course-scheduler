3f1d3eaa1a7c69c0b82fb2ac508dd2b06d32dad5
ZheyueZhao <zheyuezhao@gmail.com>
2019-12-10 20:04:21 -0500

    Add files via upload

---
+++ frontend/src/components/CourseInfo.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
+import Popup from "reactjs-popup";
+import Calendar from "../Pages/Calandar";
+
+
+
